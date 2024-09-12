9fdc39ebebba6cc659a5277cb8b3d84d0e8966ba
Fillipi <fillipi@doity.com.br>
2024-09-12 16:41:49 -0300

    validando status via api e exibindo conteudo da resposta

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-
+import requests
 
 if __name__ == "__main__":
-    print("Helloo, world!")
+    # boas vindas
+    print("Olá, mundo! \n @fillipi.dev - Python para análise de dados.")
+
+    # consultando api "economia.awesomeapi.com.br"
+    response = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    # validando alguns status
+    status_code = response.status_code
+
+    if(status_code != 200):
+        print("Falha de comunicação com a API: \n https://economia.awesomeapi.com.br")
+
+    if(status_code == 200):
+        content = response.json()
+        print(content)
